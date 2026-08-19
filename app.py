11beb438c1c20d6f995c6207aa1373a4bb6ba01d
gatsby100m <gritoujoshua66@gmail.com>
2026-08-19 16:22:51 +0100

    Fix indentation for button in app.py

--- app.py
+++ app.py
@@ -215,7 +215,7 @@ elif selected_lang == "English" and cleaned_query == "tomato foliage blighting b
         "infestation of early blight. Circular, coalescing early blight lesions. (Courtesy of Gerald Holmes, "
         "California State University, San Luis Obispo, Bugwood.org) Tomato Disease Field Guide 42 / EARLY BLIGHT"
 """             
-        with col_btn2:
+    with col_btn2:
             if st.button("Delete & Clear Inputs / Goge Bayanai", key="clear_inputs_btn"):
                 st.session_state.input_counter += 1
                 st.session_state.current_page_img = None
